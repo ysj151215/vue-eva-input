9b61d96b15b077731b9f0cd1bc87188bbec59eeb
ysj151215 <jailjt.editor@gmail.com>
2020-03-07 17:01:18 +0800

    chore: use rollup to build package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import EvaInput from "./EvaInput.vue";
 
-export default {
-  install(Vue) {
-    Vue.component("eva-input", EvaInput);
-  }
-};
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+  if (install.installed) return;
+  Vue.component("EvaInput", EvaInput);
+}
+
+// Create module definition for Vue.use()
+const plugin = { install };
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+
+if (GlobalVue) GlobalVue.use(plugin);
+
+export default EvaInput;
