b75f758c68c8a5e12b91cacc9566268ff6e3c1ed
ysj151215 <jailjt.editor@gmail.com>
2020-03-21 11:38:59 +0800

    chore: fix build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
 import vue from "rollup-plugin-vue";
 
 export default {
@@ -7,12 +8,14 @@ export default {
   output: {
     file: "dist/vue-eva-input.min.js",
     format: "umd",
+    globals: { "eva-icons": "eva" },
     name: "vue-eva-input"
   },
   plugins: [
     babel({
       babelrc: true
     }),
+    resolve({ browser: true, mainFields: ["module", "main", "jsnext:main"] }),
     vue({ css: true, compileTemplate: true })
   ]
 };
