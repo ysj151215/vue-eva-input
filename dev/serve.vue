a1cde262f1cc0877640d83a636b40dd918d4db86
ysj151215 <jailjt.editor@gmail.com>
2020-12-20 19:28:44 +0800

    chore: update bundler config

---
+++ dev/serve.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div>
+      <eva-input placeholder="Type here..." />
+    </div>
+    <div style="display: inline-block; background: #3366ff">
+      <eva-input placeholder="Type here..." primary />
+    </div>
+    <div>
+      <eva-input disabled placeholder="Disabled..." />
+    </div>
+    <div>
+      <eva-input readonly value="Readonly..." />
+    </div>
+    <div>
+      <eva-input placeholder="Type here..." status="info" suffix-icon="github" />
+    </div>
+    <div>
+      <eva-input placeholder="Type here..." status="warning" suffix-icon="github" />
+    </div>
+    <div>
+      <eva-input placeholder="Type here..." status="success" suffix-icon="github" />
+    </div>
+    <div>
+      <eva-input placeholder="Type here..." status="danger" suffix-icon="github" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import EvaInput from '@/EvaInput.vue'
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: { EvaInput },
+})
+</script>
+
+<style scoped>
+#app > div {
+  margin-bottom: 20px;
+  padding: 10px;
+}
+</style>
--- vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  productionSourceMap: false,
-  css: { extract: false },
-}
